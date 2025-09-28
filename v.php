7ddfe79dd96c786dbeef1add2a48da23ebe8aebe
Hayasaka <kgaizka77@gmail.com>
2025-09-28 19:13:02 +0700

    first commit

--- v.php
+++ v.php
@@ -1,44 +1,29 @@
 <?php
 
-// 1. Buat sebuah variabel yang berisi string JSON
+// Buat variabel berisi JSON string
 $jsonData = '{
-    "nama": "Gaizka Aga Irvanka",
-    "umur": 20,
-    "isMahasiswa": true,
-    "hobi": ["Bermain Game", "Ngoding", "Bersepeda"]
+    "nama": "Budi Santoso",
+    "umur": 25,
+    "isMahasiswa": false,
+    "hobi": ["Membaca", "Olahraga", "Memasak"]
 }';
 
-echo "<h2>Data JSON Asli:</h2>";
-echo "<pre>$jsonData</pre>";
-
-echo "<hr>";
-
-
-// 2. Decode JSON ke dalam bentuk PHP Object
-echo "<h2>1. Decode Menjadi PHP Object</h2>";
+// Decode ke PHP Object
 $phpObject = json_decode($jsonData);
 
+// Akses nilai dari PHP Object
+echo "PHP Object:<br>";
 echo "Nama: " . $phpObject->nama . "<br>";
 echo "Umur: " . $phpObject->umur . "<br>";
-echo "Hobi pertama: " . $phpObject->hobi[0] . "<br>";
+echo "Status Mahasiswa: " . ($phpObject->isMahasiswa ? "Ya" : "Tidak") . "<br>";
+echo "Hobi pertama: " . $phpObject->hobi[0] . "<br><br>";
 
-echo "<h3>Struktur Object:</h3>";
-echo "<pre>";
-print_r($phpObject);
-echo "</pre>";
+// Decode ke PHP Array (dengan argumen true)
+$phpArray = json_decode($jsonData, true);
 
-echo "<hr>";
-
-
-// 3. Decode JSON ke dalam bentuk PHP Array
-echo "<h2>2. Decode Menjadi PHP Array</h2>";
-$phpArray = json_decode($jsonData, true); 
-
-echo "Nama: " . $phpArray['Gaizka Aga Irvanka'] . "<br>";
-echo "Umur: " . $phpArray['20'] . "<br>";
-echo "Hobi kedua: " . $phpArray['Barmain Game'][1] . "<br>";
-
-echo "<h3>Struktur Array:</h3>";
-echo "<pre>";
-print_r($phpArray);
-echo "</pre>";
+// Akses nilai dari PHP Array
+echo "PHP Array:<br>";
+echo "Nama: " . $phpArray['nama'] . "<br>";
+echo "Umur: " . $phpArray['umur'] . "<br>";
+echo "Status Mahasiswa: " . ($phpArray['isMahasiswa'] ? "Ya" : "Tidak") . "<br>";
+echo "Hobi pertama: " . $phpArray['hobi'][0] . "<br>";
